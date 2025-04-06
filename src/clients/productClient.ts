2b7f488a17909b6932db36a1f77576d98496cc5f
Tam Do <tamcr94@gmail.com>
2025-04-06 16:36:54 +0700

    handle product client 401 error

--- src/clients/productClient.ts
+++ src/clients/productClient.ts
@@ -17,3 +17,17 @@ productClient.interceptors.request.use((config) => {
   }, (error) => {
     return Promise.reject(error);
   });
+
+// Add response interceptor
+productClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401 && window.location.pathname !== '/login') {
+      // Only redirect if we're not already on the login page
+      window.location.href = '/login';
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default productClient;
